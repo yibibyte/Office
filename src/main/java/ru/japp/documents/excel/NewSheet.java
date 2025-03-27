facbf79c2a87813059600970aa61ffad68bd4912
yibibyte <gnia@artcollege.ru>
2025-03-27 19:04:45 +0300

    add class NewSheet

---
+++ src/main/java/ru/japp/documents/excel/NewSheet.java
@@ -0,0 +1,49 @@
+package ru.japp.documents.excel;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class NewSheet {
+    public static void main(String[] args) throws IOException {
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet("new sheet");
+
+        // Create a row and put some cells in it. Rows are 0 based.
+        Row row = sheet.createRow(1);
+
+        // Aqua background
+        CellStyle style = wb.createCellStyle();
+        style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+        style.setFillPattern(FillPatternType.BIG_SPOTS);
+        Cell cell = row.createCell(1);
+        cell.setCellValue("X");
+        cell.setCellStyle(style);
+
+        // Orange "foreground", foreground being the fill foreground not the font color.
+        style = wb.createCellStyle();
+        style.setFillForegroundColor(IndexedColors.ORANGE.getIndex());
+        style.setFillPattern(FillPatternType.SPARSE_DOTS);
+        cell = row.createCell(2);
+        cell.setCellValue("X");
+        cell.setCellStyle(style);
+
+        // Write the output to a file
+        try (OutputStream fileOut = new FileOutputStream("workbook.xls")) {
+            try {
+                wb.write(fileOut);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        wb.close();
+    }
+}
